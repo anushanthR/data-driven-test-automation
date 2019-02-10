907490a0e8c4cb0df3b7b3ab6e690bffefdff3bb
JeyapriyaJegatheeswaran <jjypriya@gmail.com>
2019-02-10 17:02:19 +0530

    page base modify

---
+++ src/test/java/com/sgic/automation/orangehrm/utils/Test1.java
@@ -0,0 +1,35 @@
+package com.sgic.automation.orangehrm.utils;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
+public class Test1 {
+    private static final Logger LOGGER = Logger.getLogger(Test1.class);
+    @BeforeMethod
+    public void loadBrowser() {
+        LOGGER.info("Initiate Browser");
+        try {
+            PageBase.initiateDriver();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        LOGGER.info("Browser Initiated");
+    }
+    @Test
+    public void a(){
+
+    }
+
+
+    @AfterMethod
+    public void closeBrowser() {
+        LOGGER.info("Closing Browser");
+        PageBase.closeDriver();
+        LOGGER.info("Browser Closed");
+    }
+
+}
