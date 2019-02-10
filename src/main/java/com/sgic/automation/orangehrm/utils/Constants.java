c83ceae0d9f7fcfa842d0917a3ffad0b54341a1f
JeyapriyaJegatheeswaran <jjypriya@gmail.com>
2019-02-10 16:14:50 +0530

    pagebase

--- src/main/java/com/sgic/automation/orangehrm/utils/Constants.java
+++ src/main/java/com/sgic/automation/orangehrm/utils/Constants.java
@@ -1,4 +1,9 @@
 package com.sgic.automation.orangehrm.utils;
 
 public class Constants {
+    public static final String UBUNTU = "ubuntu";
+    public static final String WINDOWS = "windows";
+
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
 }
